c747bf02ca9ca1607bba68ceb20730bc94380b4d
Pushparaj Naik <pushparajnaik@Pushparajs-MacBook-Pro.local>
2025-02-16 20:27:09 +0530

    update now again new

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -8,8 +8,3 @@ resource "aws_instance" "instance" {
     Name = var.instance_name
   }
 }
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.instance.public_ip
-}

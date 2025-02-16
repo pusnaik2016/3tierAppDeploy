0c1680b4a93e09cbc04e3373ac44a7c9355fa639
Pushparaj Naik <pushparajnaik@Pushparajs-MacBook-Pro.local>
2025-02-16 20:48:00 +0530

    update now again new again

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,3 +1,6 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   tags = {
@@ -55,4 +58,31 @@ resource "aws_security_group" "db_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+#Create a security group
+resource "aws_security_group" "App_SG" {
+    #name = "App_SG"
+    description = "Allow Web inbound traffic"
+    vpc_id = aws_vpc.my_vpc.id
+    ingress  {
+        protocol = "tcp"
+        from_port = 80
+        to_port  = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress  {
+        protocol = "tcp"
+        from_port = 22
+        to_port  = 22
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress  {
+        protocol = "-1"
+        from_port = 0
+        to_port  = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
